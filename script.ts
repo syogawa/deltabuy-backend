70c79a1ca42ccb94e1429e0ffd36875c77b58e97
syogawa <bilalov734@gmail.com>
2026-08-16 02:43:23 +0500

    prisma setting

---
+++ script.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const user = await prisma.user.create({
+    data: { name: "admin" },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
