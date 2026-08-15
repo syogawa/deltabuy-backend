8874576f8e21debbe9460c38aef87aff3d2e5d05
syogawa <bilalov734@gmail.com>
2026-08-15 14:20:12 +0500

    prisma init

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
- 
