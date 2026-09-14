70354ca320743702f157b7e85c2c9b0861979222
syogawa <bilalov734@gmail.com>
2026-09-15 00:05:30 +0300

    simple auth check added, mayber will update later

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,12 +7,14 @@ import {
   Param,
   Delete,
   BadRequestException,
+  Req,
 } from "@nestjs/common";
 import { ProductService } from "./product.service";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 import { SkipThrottle } from "@nestjs/throttler";
 import { AuthController } from "../auth/auth.controller";
+import * as express from "express";
 
 @SkipThrottle({ auth: true })
 @Controller("product")
@@ -20,9 +22,13 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post("create")
-  async create(@Body() createProductDto: CreateProductDto) {
-    if (true) {
+  async create(
+    @Body() createProductDto: CreateProductDto,
+    @Req() req: express.Request,
+  ) {
+    if (req.cookies?.user) {
       return this.productService.create(createProductDto);
+      // return req.cookies?.user;
     } else {
       throw new BadRequestException("You should login first!");
     }
